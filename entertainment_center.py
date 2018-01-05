6c28cafecdcc48236020c3b9595586cd1b2286ce
Balaji Mj <mjbalaji17@gmail.com>
2018-01-05 23:22:42 +0530

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -4,16 +4,18 @@ import media
 
 interstellar = media.Movie("Interstellar",
                            "Flim about space",
-                           "http://www.imdb.com/title/tt0816692/mediaviewer/rm1728576256",
+                          "https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg",
                            "https://www.youtube.com/watch?v=Lm8p5rlrSkY")
 
 #print(interstellar.storyline)
 
 avatar = media.Movie("Avatar",
                      "A Marine on the Planet",
-                     "http://avatar.wikia.com/wiki/Category:Avatar_State_images?file=Aang_creates_an_air_pocket.png",
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=6ziBFh3V1aM")
 avatar.show_trailer()
+
+#print(avatar.storyline)
                            
 movies = [interstellar,avatar]
 fresh_tomatoes.open_movies_page(movies)
