1a9d4e9e589a030c7e6f22e44962818285f673c4
Balaji Mj <mjbalaji17@gmail.com>
2018-01-06 06:45:08 +0530

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,15 +1,19 @@
 import media
 import fresh_tomatoes
 
+#interstellar movie : movie title,storyline,poster image,movie trailer
 interstellar = media.Movie("Interstellar",
                            "Flim about space",
                            "https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg",
                            "https://www.youtube.com/watch?v=Lm8p5rlrSkY")
+#avatar movie : movie title,story line, poster image,movie trailer
 
 avatar = media.Movie("Avatar",
                      "A Marine on the Planet",
                      "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=6ziBFh3V1aM")
+
+#internship movie : movie title,story line,poster image,movie title
 
 internship = media.Movie("The Internship",
                              "Internship at Google",
@@ -19,5 +23,5 @@ internship = media.Movie("The Internship",
 
                            
 movies = [interstellar,avatar,internship]
-# fresh_tomatoes.open_movies_page(movies)
+fresh_tomatoes.open_movies_page(movies)
 print (media.Movie.__doc__)
