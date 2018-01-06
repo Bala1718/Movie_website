746b20197c23404b678f3f0e2072bd22a8fed188
Balaji Mj <mjbalaji17@gmail.com>
2018-01-06 05:47:05 +0530

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,21 +1,23 @@
-import fresh_tomatoes
 import media
-
+import fresh_tomatoes
 
 interstellar = media.Movie("Interstellar",
                            "Flim about space",
-                          "https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg",
+                           "https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg",
                            "https://www.youtube.com/watch?v=Lm8p5rlrSkY")
-
-#print(interstellar.storyline)
 
 avatar = media.Movie("Avatar",
                      "A Marine on the Planet",
                      "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=6ziBFh3V1aM")
-avatar.show_trailer()
 
-#print(avatar.storyline)
+internship = media.Movie("The Internship",
+                             "Internship at Google",
+                             "https://upload.wikimedia.org/wikipedia/en/e/ed/The-internship-poster.jpg",
+                             "https://www.youtube.com/watch?v=cdnoqCViqUo")
+
+
                            
-movies = [interstellar,avatar]
-fresh_tomatoes.open_movies_page(movies)
+movies = [interstellar,avatar,internship]
+# fresh_tomatoes.open_movies_page(movies)
+print (media.Movie.__doc__)
