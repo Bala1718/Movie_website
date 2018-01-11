b1138a8e6305a2c41f670470b1280beb3e9d5bda
Balaji Mj <mjbalaji17@gmail.com>
2018-01-11 11:27:01 +0530

    Update media.py

--- media.py
+++ media.py
@@ -2,7 +2,15 @@ import webbrowser
 
 
 class Movie():
-    """ This class provides a way to store movie information """
+    
+    """ This class provides a way to store movie information 
+    Attributes:
+        - movie_title(str):     Title of the Movie
+        - movie_storyline(str): Main Theme of the Movie
+        - poster_image(str):      Url of the video poster image
+        - trailer:   Url of the youtube trailer 
+    """
+    
     # "self" is the object or instance being created
     def __init__(self, 
                  movie_title, 
@@ -15,4 +23,5 @@ class Movie():
     # instance method
     
     def show_trailer(self):
+        """ Open the user's web browser for show the youtube movie trailer """
         webbrowser.open(self.trailer_youtube_url)
