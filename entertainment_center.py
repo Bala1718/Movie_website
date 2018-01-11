ca1c01a3d0a7f8e51053fe11647cc0805ce23cb0
Balaji Mj <mjbalaji17@gmail.com>
2018-01-11 06:58:57 +0530

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,29 +2,26 @@ import media
 import fresh_tomatoes
 
 
-#interstellar movie : movie title,storyline,poster image,movie trailer
+# interstellar movie : movie title,storyline,poster image,movie trailer
 
 interstellar = media.Movie("Interstellar",
                            "Flim about space",
-                           "https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg",
+                           "https://goo.gl/4viv4x",
                            "https://www.youtube.com/watch?v=Lm8p5rlrSkY")
 
-#avatar movie : movie title,story line, poster image,movie trailer
+# avatar movie : movie title,story line, poster image,movie trailer
 
 avatar = media.Movie("Avatar",
                      "A Marine on the Planet",
-                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://goo.gl/myzkaU",
                      "https://www.youtube.com/watch?v=6ziBFh3V1aM")
 
-#internship movie : movie title,story line,poster image,movie title
+# internship movie : movie title,story line,poster image,movie title
 
 internship = media.Movie("The Internship",
-                             "Internship at Google",
-                             "https://upload.wikimedia.org/wikipedia/en/e/ed/The-internship-poster.jpg",
-                             "https://www.youtube.com/watch?v=cdnoqCViqUo")
-
-
-                           
-movies = [interstellar,avatar,internship]
+                         "Internship at Google",
+                         "https://goo.gl/CUWEh5",
+                         "https://www.youtube.com/watch?v=cdnoqCViqUo")                        
+movies = [interstellar, avatar, internship]
 fresh_tomatoes.open_movies_page(movies)
 print (media.Movie.__doc__)
